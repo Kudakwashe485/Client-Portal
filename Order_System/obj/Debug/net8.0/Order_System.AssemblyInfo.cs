a12cdb487ef175f66e233e56c7f0b98193001796
Ntando485 <wildkuda485@gmail.com>
2024-07-14 15:32:50 +0200

    Changes

--- Order_System/obj/Debug/net8.0/Order_System.AssemblyInfo.cs
+++ Order_System/obj/Debug/net8.0/Order_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Order_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96141771105e41d7916e4fe36a66f4ce8d1a45b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4eb5355a03193c88ebf2cce9a570ca72fb51439")]
 [assembly: System.Reflection.AssemblyProductAttribute("Order_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Order_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
